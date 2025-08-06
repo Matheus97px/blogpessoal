55d0a92765b80c270ced6dc4a142d558af392e29
Matheus97px <matheus97p.q@gmail.com>
2025-08-06 16:08:12 -0300

    feat: adiciona auth e usuario com hashing de senha

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
@@ -24,4 +25,10 @@ export class Postagem {
     @JoinColumn({ name: 'tema_id' })
     tema: Tema
 
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE"
+    })
+    @JoinColumn({ name: 'usuario_id' })
+    usuario: Usuario
 }
